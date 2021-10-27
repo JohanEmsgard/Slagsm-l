706d6b3a9bf9748ee33993063562ef3dc54c1409
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2021-10-27 13:06:27 +0200

    hp

--- Slagsmål/Program.cs
+++ Slagsmål/Program.cs
@@ -13,64 +13,100 @@ while (name != "Mega Charizard X" && name != "Mega Charizard Y")
 
 
 int hpXform = 100;
-int hpYform = 100; 
+int hpYform = 100;
 
 
 while (hpXform > 0 && hpYform > 0)
 {
-if (name == "Mega Charizard X")
+    if (name == "Mega Charizard X")
+    {
+        Console.WriteLine("What Will Mega Charizard X do?");
+        Console.WriteLine("Flamethrower");
+        Console.WriteLine("Dragon Claw");
+        Console.WriteLine("Thunder Punch");
+        Console.WriteLine("Blast Burn");
+        attack = Console.ReadLine();
+    }
+
+    if (attack == "flamethrower")
+    {
+        Console.WriteLine("It's not very effective");
+        attack = Console.ReadLine();
+        hpYform -= 10;
+    }
+
+    else if (attack == "Thunder Punch")
+    {
+        Console.WriteLine("It's Super effective");
+        attack = Console.ReadLine();
+        hpYform -= 80;
+    }
+
+    else if (attack == "Dragon Claw")
+    {
+        Console.WriteLine();
+        attack = Console.ReadLine();
+        hpYform -= 40;
+    }
+
+    else if (attack == "Blast Burn")
+    {
+        Console.WriteLine("It's not very effective");
+        attack = Console.ReadLine();
+        hpYform -= 30;
+        Console.WriteLine("Mega Charizard X must recharge");
+    }
+
+    if (hpYform <= 0)
 {
-    Console.WriteLine("What Will Mega Charizard X do?");
-    Console.WriteLine("Flamethrower");
-    Console.WriteLine("Dragon Claw");
-    Console.WriteLine("Thunder Punch");
-    Console.WriteLine("Blast Burn");
-    attack = Console.ReadLine();
-    
-
+    Console.WriteLine("The Oppesing Mega Charizard Y fainted");
 }
-if ( attack == "flamethrower")
+
+else if (hpXform <= 0)
 {
-    Console.WriteLine("It's not very effective"); 
-    attack = Console.ReadLine();
-    hpYform -= 10; 
+    Console.WriteLine("Mega Charizard X fainted");
 }
 
-else if (attack == "Thunder Punch")
-{
-    Console.WriteLine("It's Super effective");
-    attack = Console.ReadLine();
-    hpYform -=80;
+    else if (name == "Mega Charizard Y")
+    {
+        Console.WriteLine("What Will Mega Charizard Y do?");
+        Console.WriteLine("Fire Spin");
+        Console.WriteLine("Dragon Claw");
+        Console.WriteLine("Heat Wave");
+        Console.WriteLine("Dragon Tail");
+        attack = Console.ReadLine();
+
+    }
+
+    if (attack == "Fire Spin")
+    {
+        Console.WriteLine("It's not very effective");
+        attack = Console.ReadLine();
+        hpXform -= 10;
+    }
+
+    else if (attack == "Dragon Claw")
+    {
+        Console.WriteLine("It's Super effective");
+        attack = Console.ReadLine();
+        hpXform -= 50;
+    }
+
+    else if (attack == "Heat Wave")
+    {
+        Console.WriteLine("It's not very effective");
+        attack = Console.ReadLine();
+        hpXform -= 30;
+    }
+
+    else if (attack == "Dragon Tail")
+    {
+        Console.WriteLine("It's super effective");
+        attack = Console.ReadLine();
+        hpXform -= 45;
+    }
+
+    Console.ReadLine();
 }
-
-else if (attack == "Dragon Claw")
-{
-    Console.WriteLine();
-    attack = Console.ReadLine();
-    hpYform -=40;
-}
-
-else if (attack == "Blast Burn")
-{
-    Console.WriteLine("It's not very effective");
-    attack = Console.ReadLine();
-    hpYform -=30;
-    Console.WriteLine("Mega Charizard X must recharge");
-}
-
-
-else
-{
-    Console.WriteLine("What Will Mega Charizard Y do?");
-    Console.WriteLine("Fire Spin");
-    Console.WriteLine("Dragon Claw");
-    Console.WriteLine("Heat Wave");
-    Console.WriteLine("Dragon Tail");
-}
-
-
-Console.ReadLine(); 
-}
-
 
 Console.ReadLine();
