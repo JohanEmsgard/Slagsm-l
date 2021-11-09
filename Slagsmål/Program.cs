450e93a4fcd164ad62f44bc2c10a2ae06c4dd431
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2021-11-09 14:51:20 +0100

    jfs

--- Slagsmål/Program.cs
+++ Slagsmål/Program.cs
@@ -15,6 +15,7 @@ while (name != "X" && name != "Y")
 int hpXform = 100;
 int hpYform = 100;
 
+Random generator = new Random();
 
 while (hpXform > 0 && hpYform > 0)
 {
@@ -26,38 +27,56 @@ while (hpXform > 0 && hpYform > 0)
         Console.WriteLine("3) Thunder Punch");
         Console.WriteLine("4) Blast Burn");
         attack = Console.ReadLine();
+
+        if (attack == "1")
+        {
+            Console.WriteLine("It's not very effective");
+            attack = Console.ReadLine();
+            hpYform -= 10;
+        }
+
+        else if (attack == "3")
+        {
+            Console.WriteLine("It's Super effective");
+            attack = Console.ReadLine();
+            hpYform -= 80;
+        }
+
+        else if (attack == "2")
+        {
+            Console.WriteLine();
+            attack = Console.ReadLine();
+            hpYform -= 40;
+        }
+
+        else if (attack == "4")
+        {
+            Console.WriteLine("It's not very effective");
+            attack = Console.ReadLine();
+            hpYform -= 30;
+            Console.WriteLine("Mega Charizard X must recharge");
+        }
+
+        int enemyAction = generator.Next(4);
+        if (enemyAction == 0)
+        {
+            Console.WriteLine("Mega Charizard Y used Fire Spin");
+            hpXform -= 10;
+        } 
+
+        else 
+        {
+            Console.WriteLine("Mega Charizard Y used Dragon Claw");
+            hpXform -= 50;
+        }     
+
+        else
+        {
+            Console.WriteLine("Mega Charizard Y used Heat Wave");
+        }
+
     }
 
-    if (attack == "1")
-    {
-        Console.WriteLine("It's not very effective");
-        attack = Console.ReadLine();
-        hpYform -= 10;
-    }
-
-    else if (attack == "3")
-    {
-        Console.WriteLine("It's Super effective");
-        attack = Console.ReadLine();
-        hpYform -= 80;
-    }
-
-    else if (attack == "2")
-    {
-        Console.WriteLine();
-        attack = Console.ReadLine();
-        hpYform -= 40;
-    }
-
-    else if (attack == "4")
-    {
-        Console.WriteLine("It's not very effective");
-        attack = Console.ReadLine();
-        hpYform -= 30;
-        Console.WriteLine("Mega Charizard X must recharge");
-    }
-
-    
 
     else if (name == "Y")
     {
